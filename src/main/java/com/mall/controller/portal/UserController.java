493074578e3e7b4847fa02ead9b6f85170702167
zhousheng <zhousheng841536@outlook.com>
2020-05-09 20:53:50 +0800

    1

--- src/main/java/com/mall/controller/portal/UserController.java
+++ src/main/java/com/mall/controller/portal/UserController.java
@@ -44,7 +44,7 @@ public class UserController {
         return response;
     }
 
-    @RequestMapping(value = "logout.do",method = RequestMethod.GET)
+    @RequestMapping(value = "logout.do",method = RequestMethod.POST)
     @ResponseBody
     public ServerResponse<String> logout(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse){
         String loginToken = CookieUtil.readLoginToken(httpServletRequest);
